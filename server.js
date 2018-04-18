6adfd37a772f2f4cbb61e574c5c95c28373fd490
EstrellaBF <stern.von.nord@gmail.com>
2018-04-18 10:35:52 -0500

    borrando server

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const server = app.listen(3000, turnOn);
-
-function turnOn() {
- console.log('Servidor encendido');
-}
-
-app.use(express.static('public'));
